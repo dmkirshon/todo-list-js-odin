b321be3f712507694a9995467ec3291d0907c67b
dmkirshon <kirshon.david@gmail.com>
2022-02-16 17:19:28 +0200

    feat: Add visual settings button icon

--- src/index.js
+++ src/index.js
@@ -4,28 +4,48 @@ const content = document.createElement('div');
 
 function createHeader() {
     const header = document.createElement('header');
-    const projectsListLabel = document.createElement('label');
-    const projectsListDropDown = document.createElement('select');
-    const addProjectButton = document.createElement('button');
-
-    projectsListLabel.setAttribute('for', 'projects');
-    projectsListDropDown.setAttribute('id', 'projects');
-    projectsListDropDown.setAttribute('name', 'projectsList');
-
-    projectsListDropDown.replaceChildren(...displayController.displayProjectsSelectOptions());
     
-    addProjectButton.textContent = '+';
-    addProjectButton.onclick = addProject;
+    function createProjectsListDiv() {
+        const projectsDiv = document.createElement('div');
+        const projectsListLabel = document.createElement('label');
+        const projectsListDropDown = document.createElement('select');
+        const addProjectButton = document.createElement('button');
+        const projectOptions = displayController.displayProjectsSelectOptions();
+        
+        projectsListLabel.setAttribute('for', 'projects');
+        projectsListDropDown.setAttribute('id', 'projects');
+        projectsListDropDown.setAttribute('name', 'projectsList');
 
-    function addProject() {
-       const newProjectName = prompt('What is the name of your new project?');
-       displayController.captureNewProject(newProjectName);
-       
-       projectsListDropDown.replaceChildren(...displayController.displayProjectsSelectOptions());
+        projectsListDropDown.replaceChildren(...projectOptions);
+        
+        addProjectButton.textContent = '+';
+
+        addProjectButton.onclick = () => {
+            const newProjectName = prompt('What is the name of your new project?');
+            displayController.captureNewProject(newProjectName);
+            
+            projectsListDropDown.replaceChildren(...projectOptions);
+        }
+        
+        projectsDiv.appendChild(projectsListDropDown);
+        projectsDiv.appendChild(addProjectButton);
+
+        return projectsDiv;
     }
 
-    header.appendChild(projectsListDropDown);
-    header.appendChild(addProjectButton);
+    function createSettingsButtonDiv() {
+        const settingsDiv = document.createElement('div');
+        const settingsButton = document.createElement('button');
+
+        settingsButton.textContent = '\u2699';
+
+        settingsDiv.appendChild(settingsButton);
+        
+        return settingsDiv;
+    }
+
+    header.appendChild(createProjectsListDiv());
+    header.appendChild(createSettingsButtonDiv());
 
     return header;
 };
